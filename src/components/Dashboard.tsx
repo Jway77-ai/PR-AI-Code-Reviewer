d40a31e3978b9c6e1c661de6b8af122ae8d4caf5
helmyrsln <helmyrsln@Helmys-MacBook-Air.local>
2024-09-14 19:26:47 +0800

    frontend: removed unused

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FaExternalLinkAlt, FaTrash } from "react-icons/fa";
 import Link from "next/link";
 
