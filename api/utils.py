6e7ac1288bf618f891a57d56e4c59c723ad9f009
Glendon Chua <glendonchua@gmail.com>
2024-09-14 14:01:53 +0800

    Backend: Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -55,7 +55,7 @@ def get_files_diff(pr_id):
         BITBUCKET_REPO_SLUG = os.getenv("BITBUCKET_REPO_SLUG")
 
         # URL to get the pull request diff
-        url = f"https://api.bitbucket.org/2.0/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/pullrequests/{pr_id}/diff"
+        url = f"https://api.bitbucket.org/2.0/repositories/{BITBUCKET_WORKSPACE}/{BITBUCKET_REPO_SLUG}/pullrequests/7/diff"
         headers = {'Authorization': f'Bearer {access_token}'}
 
         # Make the request to get the pull request diff
